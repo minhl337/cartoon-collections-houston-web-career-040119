70ec1a11443c61d06780ef556350741984612cdd
minh nguyen <imnotminh@gmail.com>
2019-03-27 00:31:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   
   array.each_with_index do |thing,i|
-    puts "#{i}.#{thing}"
+    puts "#{i+1}.#{thing}"
   end
   
 end
